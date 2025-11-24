db14d1e2ee5035a86575dc28be93574c90d11404
Priyanshu <126237755+Piku-Code@users.noreply.github.com>
2025-11-24 23:29:28 +0530

    update vite config

--- PostCraft-AI/client/vite.config.js
+++ PostCraft-AI/client/vite.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+        allowedHosts: ["postcraft-ai-npg6.onrender.com"]    
+  }
   server: {
     port: 3000,
     proxy: {
